e8e337e9269db8ee41b933924e7b0c1bff1cda5d
Abhijeetrawat2304 <rawatabhijeet20@gmail.com>
2026-09-02 21:33:22 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (74.15%) - LeetSync

---
+++ 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.c
@@ -0,0 +1,21 @@
+int timeRequiredToBuy(int* tickets, int ticketsSize, int k) {
+       int time = 0;
+
+    for (int i = 0; i < ticketsSize; i++) {
+        if (i <= k) {
+            if (tickets[i] < tickets[k])
+                time += tickets[i];
+            else
+                time += tickets[k];
+        }
+        else {
+            if (tickets[i] < tickets[k])
+                time += tickets[i];
+            else
+                time += tickets[k] - 1;
+        }
+    }
+
+    return time;
+
+}
